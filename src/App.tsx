3b477458701107ccc2f5900347ffbe3f6f9b5d8a
Brian Chan <brianc42@cs.washington.edu>
2019-11-03 03:18:56 -0800

    Standardize indentation

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Counter from './Counter'
 import ClockApp from './Clock'
 
 const App = () => (
-  <div>
-    <h1>Hello, world.</ h1>
-    <Counter/>
-    clock!
-    <ClockApp />
-  </div>
+	<div>
+		<h1>Hello, world.</ h1>
+		<Counter/>
+		clock!
+		<ClockApp />
+	</div>
 )
 
 export default hot(App)
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,35 +1,35 @@
 import * as React from 'react'
 
 class Counter extends React.Component<{}, { count: number }> {
-  interval: number
+	interval: number
 
-  constructor(props : any) {
-    super(props)
-    this.state = { count: 0 }
-  }
+	constructor(props : any) {
+		super(props)
+		this.state = { count: 0 }
+	}
 
-  componentDidMount() {
-    this.interval = window.setInterval(
-      () => this.setState(prevState => ({ count: prevState.count + 1 })),
-      200,
-    )
-  }
+	componentDidMount() {
+		this.interval = window.setInterval(
+			() => this.setState(prevState => ({ count: prevState.count + 1 })),
+			200,
+		)
+	}
 
-  generateString1() {
-    return "2"
-  }
+	generateString1() {
+		return "2"
+	}
 
-  generateString2 = ()  => {
-    return "1"
-  }
+	generateString2 = ()  => {
+		return "1"
+	}
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
+	componentWillUnmount() {
+		clearInterval(this.interval)
+	}
 
-  render() {
-    return <span>{this.state.count} - {this.generateString1()} - {this.generateString2()} ({this.interval})</span>
-  }
+	render() {
+		return <span>{this.state.count} - {this.generateString1()} - {this.generateString2()} ({this.interval})</span>
+	}
 }
 
 export default Counter
